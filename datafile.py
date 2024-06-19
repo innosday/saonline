113402ce1a59bff4c7d4779e9c8314b0009461c4
innosday <162128735+innosday@users.noreply.github.com>
2024-06-19 23:35:21 +0900

    애니메이션 띄우기 성공

--- datafile.py
+++ datafile.py
@@ -7,12 +7,10 @@ class SpriteSheet:
     def __init__(self, filename, width, height, max_row, max_col, max_index):
         baseImage = pygame.image.load(os.path.join(DIR_IMAGE, filename)).convert()
         self.spr = []
-        self.width = width
-        self.height = height
 
         for i in range(max_index):      # 스프라이트 시트의 각 인덱스에 자른 이미지 저장
             image = pygame.Surface((width, height))
             image.blit(baseImage, (0, 0), 
                        ((i % max_row) * width, (i // max_col) * height, width, height))
-            image.set_colorkey((0, 0, 0))
-            self.spr.append(image)
+            image_scaled = pygame.transform.scale(image,(width * 4,height * 4))
+            self.spr.append(image_scaled)
--- main.py
+++ main.py
@@ -6,7 +6,19 @@ from pygame.locals import *
 clock = pygame.time.Clock()
 pygame.init()
 
-screen = [800,600]
-pygame.display.set_mode(screen)
+screen = [640,480]
+bg = pygame.display.set_mode(screen,0,32)
 
-spr_character = SpriteSheet('')
+spr_character = SpriteSheet('IDLE.png',24,40,5,34,5)
+# spr_character = SpriteSheet('SpriteSheet1.png',16,16,8,8,11)
+
+while True:
+    bg.blit(spr_character.spr[1],(320,240))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
